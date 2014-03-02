267ae5c6bba9e9d701f11742eb1875662f3285f2
Kaitlin <kaitlincarpenter@icloud.com>
2014-03-01 19:03:55 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gracie::Application.config.secret_key_base = '862ba235cc6b27a5137df191f48edb202eaca013a762abc88dde1004f4bb7a39e77a16b8dd86ce4b035f8515e13e37a58449d147726b8b2ebe1b2af4599d2101'
